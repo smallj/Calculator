33d2351d77059f5c3fc44160b309923c3b1aa612
Jim Small <smallj@live.com>
2016-11-10 21:46:29 -0500

    started adding button functionality

--- app/src/main/java/com/smallnetworks/calculator/MainActivity.java
+++ app/src/main/java/com/smallnetworks/calculator/MainActivity.java
@@ -8,8 +8,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int total;
-    private int lastNumber;
+    private Integer total = 0;
+    private Integer lastTotal = 0;
+    private String inputString = "";
     private Button btnClearAll;
     private Button btnClearLast;
     private Button btnBackSpace;
@@ -27,14 +28,14 @@ public class MainActivity extends AppCompatActivity {
     private Button btnDouble0;
     private Button btnMinusEqual;
     private Button btnPlusEqual;
-    private TextView textTotal;
+    private TextView txtTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textTotal = (TextView) findViewById(R.id.textResult);
+        txtTotal = (TextView) findViewById(R.id.txtTotal);
         btnClearAll = (Button) findViewById(R.id.btnClearAll);
         btnClearLast = (Button) findViewById(R.id.btnClearLast);
         btnBackSpace = (Button) findViewById(R.id.btnBackSpace);
@@ -53,17 +54,140 @@ public class MainActivity extends AppCompatActivity {
         btnMinusEqual = (Button) findViewById(R.id.btnMinusEqual);
         btnPlusEqual = (Button) findViewById(R.id.btnPlusEqual);
 
+        txtTotal.setText("0");
+
         btnClearAll.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textTotal.setText("0.00");
+                total = 0;
+                txtTotal.setText("0");
             }
         });
 
         btnClearLast.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textTotal.setText("0.00");
+                total = lastTotal;
+                txtTotal.setText(total.toString());
+            }
+        });
+
+        btnBackSpace.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (inputString.length() > 0)
+                    inputString = inputString.substring(0, inputString.length() - 1);
+                txtTotal.setText(inputString);
+            }
+        });
+
+        btn0.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputString += "0";
+                txtTotal.setText(inputString);
+            }
+        });
+
+        btn1.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputString += "1";
+                txtTotal.setText(inputString);
+            }
+        });
+
+        btn2.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputString += "2";
+                txtTotal.setText(inputString);
+            }
+        });
+
+        btn3.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputString += "3";
+                txtTotal.setText(inputString);
+            }
+        });
+
+        btn4.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputString += "4";
+                txtTotal.setText(inputString);
+            }
+        });
+
+        btn5.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputString += "5";
+                txtTotal.setText(inputString);
+            }
+        });
+
+        btn6.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputString += "6";
+                txtTotal.setText(inputString);
+            }
+        });
+
+        btn7.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputString += "7";
+                txtTotal.setText(inputString);
+            }
+        });
+
+        btn8.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputString += "8";
+                txtTotal.setText(inputString);
+            }
+        });
+
+        btn9.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputString += "9";
+                txtTotal.setText(inputString);
+            }
+        });
+
+        btnPoint.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputString += ".";
+                txtTotal.setText(inputString);
+            }
+        });
+
+        btnMinusEqual.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int tmp = Integer.parseInt(inputString);
+                lastTotal = total;
+                total -= tmp;
+                txtTotal.setText(total.toString());
+                inputString = "";
+            }
+        });
+
+        btnPlusEqual.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int tmp = Integer.parseInt(inputString);
+                lastTotal = total;
+                total += tmp;
+                txtTotal.setText(total.toString());
+                inputString = "";
             }
         });
     }
